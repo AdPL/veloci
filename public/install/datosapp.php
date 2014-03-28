0befbbf795c42f29221808cea52e85302d5fee63
Adrián Pérez López <adrianpelopez@gmail.com>
2014-03-28 15:47:00 +0100

    Added license

--- public/install/datosapp.php
+++ public/install/datosapp.php
@@ -1,4 +1,19 @@
 <?php
+	/*  VELOCI - Web application for management races
+	Copyright (C) 2014: Adrián Pérez López
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see [http://www.gnu.org/licenses/]. */
 	$todocorrecto = false; // Variable que nos permitira continuar la instalación o no.
 
 	// Busca si existe un fichero de configuración y si existe, detiene todo proceso de instalación.
