4f02f65ab439896a2305db35238bcb72c5984ce4
Adrián Pérez López <adrianpelopez@gmail.com>
2014-04-04 17:59:39 +0200

    Added register function
    Registro de usuarios funcionando

--- routes/usuario.php
+++ routes/usuario.php
@@ -18,3 +18,38 @@
 $app->get('/login', function() use ($app) {
     $app->render('login.html.twig');
 })->name('login');
+
+$app->get('/registro', function() use ($app) {
+    if(isset($_SESSION['id'])) {
+        $app->redirect($app->urlFor('principal'));
+    }
+    
+    $app->render('registro.html.twig');
+})->name('registro');
+
+$app->post('/registro', function() use ($app) {
+    if(registrarUsuario($app, $_POST['inputUsuario'], $_POST['inputEmail'], $_POST['inputPassword'], $_POST['inputPassword2'], $_POST['inputNombreCompleto'])) {
+        // MENSAJE DE REGISTRO CORRECTO
+    } else {
+        // MENSAJE DE REGISTRO INCORRECTO
+    }
+    $app->render('registro.html.twig');
+})->name('registroUsuario');
+
+function registrarUsuario($app, $usuario, $email, $password, $passwordCheck, $nombreCompleto) {
+    if ($password == $passwordCheck) {
+        $user = ORM::for_table('piloto')->create();
+        $user->id = null;
+        $user->nombre = strtolower($usuario);;
+        $user->password = password_hash($password, PASSWORD_DEFAULT);
+        $user->escuderia = 'Ninguna';
+        $user->nombre_completo = $nombreCompleto;
+        $user->email = $email;
+        $user->rol = 1;
+        $user->save();
+
+        return true;   
+    } else {
+        return false;
+    }
+}
