cd4d6a320c5c6b01152ad837a2668e99b3e594f4
Adrián Pérez López <adrianpelopez@gmail.com>
2014-04-26 13:28:20 +0200

    Added popover function and improved calendar
    Ahora el calendario también puede ser generado para un día o mes
    especifico, o ambos.

--- public/js/jsfunctions.js
+++ public/js/jsfunctions.js
@@ -1,9 +1,27 @@
-function generarCalendario() {
-	var dias = new Array('L', 'M', 'X', 'J', 'V', 'S', 'D');
+function generarCalendario(dia, mes) {
 	var fecha = new Date();
-	var hoy = fecha.getDate();
+	var dias = new Array('L', 'M', 'X', 'J', 'V', 'S', 'D');
+	var diasMes = new Array('31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31');
+
+	if (dia != null) {
+		fecha.setDate(dia);
+		hoy = dia;
+	} else {
+		var hoy = fecha.getDate();
+	}
+
+	if (mes != null) {
+		mes--;
+		fecha.setMonth(mes);
+	} else {
+		mes = fecha.getMonth();
+	}
+
+	// Localizo el día del comienzo del mes
 	fecha.setDate(1);
 	var diaUno = fecha.getDay();
+
+	// Inicializo variables
 	var i = 1;
 	var j = 1;
 	var cadena = "";
@@ -21,20 +39,26 @@ function generarCalendario() {
 		i++;
 	}
 
-	while (j <= 31) {
+	while (j <= diasMes[mes]) {
 		if (i == 7) {
 			cadena += "</tr><tr>";
 			i = 0;
 		};
 		
 		if(j == hoy)
-			cadena += "<td class='info' aling='right'>" + j + "</td>";
+			cadena += "<td class='info' aling='right' data-container='body' data-placement='top' data-toggle='popover' title='Día " + j + " de marzo' data-content='20:00  -  Gran Premio de Montmelo<br/>23:00  -  Gran Premio de China'><a href='#'>" + j + "</a></td>";
 		else
 			cadena += "<td align='right'>" + j + "</td>";
 
 		j++;
 		i++;
 	}
+
+	while (i < 7) {
+		cadena += "<td></td>";
+		i++;
+	}
+
 	cadena += "</table>"
 
 	document.getElementById('calendario').innerHTML = cadena;
