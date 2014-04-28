7decc92f80a4efc885d9e6be3d6a2c6caba919f2
Adrián Pérez López <adrianpelopez@gmail.com>
2014-04-28 18:08:06 +0200

    Updated templates
    Agregadas distintas funciones, preparación de plantillas y visualización
    de la próxima carrera en la principal

--- routes/principal.php
+++ routes/principal.php
@@ -16,10 +16,12 @@
   along with this program.  If not, see [http://www.gnu.org/licenses/]. */
 
 $app->get('/', function() use ($app) {
+	$carrera = cargarCarrera();
+
 	if(!isset($_SESSION['id'])) {
 		$app->render('principal.html.twig');
 	} else {
-		$app->render('principal.html.twig', array('id' => $_SESSION['id'], 'usuario' => $_SESSION['nombre_completo'], 'avatar' => $_SESSION['avatar'], 'rol' => $_SESSION['rol']));
+		$app->render('principal.html.twig', array('id' => $_SESSION['id'], 'usuario' => $_SESSION['nombre_completo'], 'avatar' => $_SESSION['avatar'], 'rol' => $_SESSION['rol'], 'carrera' => $carrera));
 	}
 })->name('principal');
 
@@ -61,6 +63,26 @@ $app->get('/categorias', function() use ($app) {
 	}
 })->name('categorias');
 
+$app->get('/carreras', function() use ($app) {
+	$carreras = cargarCarreras();
+
+	if(!isset($_SESSION['id'])) {
+		$app->render('carreras.html.twig', array('carreras' => $carreras));
+	} else {
+		$app->render('carreras.html.twig', array('id' => $_SESSION['id'], 'usuario' => $_SESSION['nombre_completo'], 'avatar' => $_SESSION['avatar'], 'rol' => $_SESSION['rol'], 'carreras' => $carreras));
+	}
+})->name('carreras');
+
+$app->get('/reclamaciones', function() use ($app) {
+	$carreras = cargarCarrerasReclamacion();
+
+	if(!isset($_SESSION['id'])) {
+		$app->render('reclamaciones.html.twig', array('carreras' => $carreras));
+	} else {
+		$app->render('reclamaciones.html.twig', array('id' => $_SESSION['id'], 'usuario' => $_SESSION['nombre_completo'], 'avatar' => $_SESSION['avatar'], 'rol' => $_SESSION['rol'], 'carreras' => $carreras));
+	}
+})->name('reclamaciones');
+
 function testAccess($app, $usuario, $pass) {
 	$user = ORM::for_table('piloto')->where('nombre', $usuario)->find_one();
 	if ($user['nombre'] == $usuario && password_verify($pass, $user['password'])) {
@@ -72,4 +94,28 @@ function testAccess($app, $usuario, $pass) {
 	} else {
 		return false;
 	}
+}
+
+function cargarCarrera() {
+	return ORM::for_table('carrera')->where_gt('fecha', date("Y-m-d"))->order_by_asc('fecha')->limit(1)->find_many();
+}
+
+function cargarCarrerasReclamacion() {
+    return ORM::for_table('carrera')->where_gt('fecha', date("Y-m-d"))->where_lt('fecha', calcularFecha('days', 4, date("Y-m-d")))->
+    order_by_asc('fecha')->find_many();
+}
+
+function calcularFecha($modo, $valor, $fecha_inicio = false){
+ 
+   if ($fecha_inicio != false) {
+          $fecha_base = strtotime($fecha_inicio);
+   } else {
+          $time = time();
+          $fecha_actual = date("Y-m-d",$time);
+          $fecha_base = strtotime($fecha_actual);
+   }
+ 
+   $calculo = strtotime("$valor $modo","$fecha_base");
+ 
+   return date("Y-m-d", $calculo);
 }
