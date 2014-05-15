f13a99c6d375fa8016a8c9030b748f54b29df6db
Adrián Pérez López <adrianpelopez@gmail.com>
2014-05-15 19:43:33 +0200

    Added check user system

--- public/js/jsfunctions.js
+++ public/js/jsfunctions.js
@@ -1,3 +1,19 @@
+/*  VELOCI - Web application for management races
+	Copyright (C) 2014: Adrián Pérez López
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see [http://www.gnu.org/licenses/]. */
+
 $('#registerModal').click( function() {
 	$('#registerModal').modal();
 });
@@ -72,3 +88,19 @@ function generarCalendario(dia, mes) {
 	document.getElementById('calendario').innerHTML = cadena;
 }
 
+function compruebaNombre(valor)
+{
+    var request;
+    request = $.ajax({
+        url: "/ajax.php",
+        type: "GET",
+        data: {val: valor}
+    });
+
+    request.done(function(response, textStatus, jqXHR)
+    {
+        document.getElementById("pepito").className = response;
+        document.getElementById("pepe").innerHTML = response;
+
+    });
+}
